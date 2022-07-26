a0af5b8c141a2e27f7420021fc285ebe7f621545
Spencer Ferguson-Dryden <sfergusond@gmail.com>
2022-07-26 15:11:47 -0400

    Added support for local mirror of args file

--- proposalDownload.py
+++ proposalDownload.py
@@ -58,5 +58,8 @@ def run(**kwargs):
   print('Proposals written to data/lfc-proposals.csv')
 
 if __name__ == '__main__':
-  kwargs = json.load(open('args.json'))
+  try:
+    kwargs = json.load(open('args.local.json'))
+  except:
+    kwargs = json.load(open('args.json'))
   run(**kwargs)
